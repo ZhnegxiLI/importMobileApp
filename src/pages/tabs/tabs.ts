0986c36bfab12a8a50630a9a173da94f17605bf3
martin <zhengxi.li.19@neoma-bs.com>
2020-02-19 23:11:38 +0100

    加入订单确认页面

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,11 +1,4 @@
 import { Component } from '@angular/core';
-
-import { HomePage } from '../home/home';
-// import { DiscoveryPage } from '../discovery/discovery';
-import { CartPage } from '../cart/cart';
-import { NotificationPage } from '../notification/notification'; 
-import {RegistreSuccedPage} from '../registre-succed/registre-succed'
-import { ReadOrderListPage } from '../read-order-list/read-order-list';
 import { IonicPage } from 'ionic-angular';
 
 @IonicPage()
@@ -17,7 +10,7 @@ export class TabsPage {
   tabHome = 'HomePage';
   tabDiscovery = 'ReadOrderListPage';
   tabCart = 'CartPage';
-  tabNotification = 'RegistreSuccedPage';
+  tabNotification = 'OrderConfirmationPage';
   tabMyAccount = 'MyAccountPage';
 
   constructor() {
