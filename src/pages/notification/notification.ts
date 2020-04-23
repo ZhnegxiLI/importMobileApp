0af5165691b7894aff9fcb7daff8b98c9ccf6927
martin <zhengxi.li.19@neoma-bs.com>
2020-04-23 15:46:15 +0200

    完善notice模块

--- src/pages/notification/notification.ts
+++ src/pages/notification/notification.ts
@@ -14,10 +14,12 @@ import { Events } from 'ionic-angular';
 })
 export class NotificationPage extends BaseUI {
 
+  public counter: number = 0;
+  public step: number = 15;
   public messageList: any[] = [];
   public loading: boolean = false;
   constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public translateService: TranslateService,
-    public toastCtrl: ToastController, public rest: RestProvider, public network: Network, public modalCtrl: ModalController,public event: Events) {
+    public toastCtrl: ToastController, public rest: RestProvider, public network: Network, public modalCtrl: ModalController, public event: Events) {
     super();
   }
 
@@ -31,6 +33,8 @@ export class NotificationPage extends BaseUI {
     if (this.network.type != 'none') {
       var criteria = {
         UserId: localStorage.getItem('userId'),
+        Step: this.step,
+        Begin: this.counter
       }
       this.loading = true
       this.rest.GetMessageByUserAndStatus(criteria) // 填写url的参数
@@ -38,8 +42,8 @@ export class NotificationPage extends BaseUI {
           result => {
             if (result.List != null && result.List.length > 0) {
               this.messageList = result.List;
-              
-              var count = this.messageList.filter(p=>p.IsReaded==false);
+
+              var count = this.messageList.filter(p => p.IsReaded == false);
               this.event.publish('message:new', count);
             }
 
@@ -63,9 +67,44 @@ export class NotificationPage extends BaseUI {
     modal.present();
     modal.onDidDismiss(f => {
       this.loadMesage();
-    })
+    });
 
   }
+
+
+  doInfinite(infiniteScroll) {
+    if (this.network.type != 'none') {
+      this.counter = this.counter + 1;
+      var criteria = {
+        UserId: localStorage.getItem('userId'),
+        Step: this.step,
+        Begin: this.counter
+      }
+      this.rest.GetMessageByUserAndStatus(criteria) 
+        .subscribe(
+          (result: any) => {
+            if (result!=null && result.List!= null) {
+              if (result.TotalCount <= this.step * this.counter) {
+                infiniteScroll.enable(false);   //没有数据的时候隐藏 ion-infinate-scroll
+              }
+              else {
+                this.messageList = this.messageList.concat(result.List!= null ? result.List: []);
+                infiniteScroll.complete();
+              }
+            } else {
+              super.showToast(this.toastCtrl, this.translateService.instant("Msg_Error"));
+            }
+          },
+          error => {
+            super.showToast(this.toastCtrl, this.translateService.instant("Msg_Error"));
+          }
+        );
+    }
+    else {
+      super.showToast(this.toastCtrl, this.translateService.instant("Msg_Offline"));
+    }
+  }
+
 
 
 }
