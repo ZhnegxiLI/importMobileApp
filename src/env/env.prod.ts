7bb790dc2fabc12bd3b0798620a732ab2595f4f3
martin <zhengxi.li.19@neoma-bs.com>
2020-07-06 09:45:30 +0200

    add prod url

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -1,5 +1,5 @@
 export class ENV {
-    public static SERVER_API_URL = "http://47.100.137.77/";
+    public static SERVER_API_URL = 'https://jlsprod.europetechs.com/';
     public static MODE = 'PROD';
     public static LABEL = '正式版';
     public static HTTP_WAITING_TIME = 20000;
