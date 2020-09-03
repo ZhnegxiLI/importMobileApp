b638b943a081c592d72b6e5d97951419698f4a9e
zhengxili <zhengxi.li.19@neoma-bs.com>
2020-09-03 22:49:51 +0200

    add icon

--- src/env/env.dev.ts
+++ src/env/env.dev.ts
@@ -1,5 +1,5 @@
 export class ENV {
-    public static SERVER_API_URL ="http://localhost/JLSMobileApplication/" ;//; "http://jlsmobile.europetechs.com/"
+    public static SERVER_API_URL ="https://localhost:44304/" ;//; "https://localhost:44304/"
     public static MODE = 'DEV';
     public static LABEL = '开发版';
     public static HTTP_WAITING_TIME = 200000;
